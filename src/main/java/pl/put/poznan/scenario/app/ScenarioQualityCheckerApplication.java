85a9068323e296e4f432bd041847539f28b45ab6
anffie <36477661+anffie@users.noreply.github.com>
2019-11-25 19:04:24 +0100

    Added JSONfileReader class

--- src/main/java/pl/put/poznan/scenario/app/ScenarioQualityCheckerApplication.java
+++ src/main/java/pl/put/poznan/scenario/app/ScenarioQualityCheckerApplication.java
@@ -2,6 +2,7 @@ package pl.put.poznan.scenario.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pl.put.poznan.scenario.logic.JSONfileReader;
 
 
 @SpringBootApplication(scanBasePackages = {"pl.put.poznan.scenario.rest"})
@@ -10,7 +11,8 @@ public class ScenarioQualityCheckerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ScenarioQualityCheckerApplication.class, args);
         System.out.println("\n-----> Scenario Quality Checker <-----\n");
-
+        String JSONfile = new JSONfileReader().toString("vehicle_inspection_station_scenario.json");
+        System.out.println(JSONfile);
     }
 
 }
---
+++ src/main/java/pl/put/poznan/scenario/logic/JSONfileReader.java
@@ -0,0 +1,36 @@
+package pl.put.poznan.scenario.logic;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class JSONfileReader {
+
+    private File getFileFromResources(String fileName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+
+        URL resource = classLoader.getResource(fileName);
+        if (resource == null) {
+            throw new IllegalArgumentException("file is not found!");
+        } else {
+            return new File(resource.getFile());
+        }
+
+    }
+
+    public String toString(String filename) {
+
+        String filepath = getFileFromResources(filename).getPath();
+        String content = "";
+        try {
+            content = new String ( Files.readAllBytes( Paths.get(filepath) ) );
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        return content;
+
+    }
+}
