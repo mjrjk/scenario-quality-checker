269c19d9cd79eb1a94bc68d1a752af8de15e3493
anffie <36477661+anffie@users.noreply.github.com>
2019-11-26 19:40:58 +0100

    Added REST Controller

--- src/main/java/pl/put/poznan/scenario/rest/ScenarioQualityCheckerController.java
+++ src/main/java/pl/put/poznan/scenario/rest/ScenarioQualityCheckerController.java
@@ -1,13 +1,38 @@
 package pl.put.poznan.scenario.rest;
 
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import pl.put.poznan.scenario.logic.JSONfileReader;
+import pl.put.poznan.scenario.logic.JSONtoObject;
+import pl.put.poznan.scenario.logic.ScenarioQualityChecker;
+import pl.put.poznan.scenario.model.Scenario;
+
 
 @RestController
 public class ScenarioQualityCheckerController {
-    @RequestMapping(method = RequestMethod.GET, path = "/hello")
-    public String satHello() {
-        return "Hello world";
+
+    @RequestMapping(method = RequestMethod.GET, path = "/all-steps-count/{filename}")
+    public int countAllSteps(@PathVariable String filename) {
+        String JSONfile = new JSONfileReader().toString(filename);
+
+        if(JSONfile == "{}" || JSONfile == "")
+            return 0;
+
+        Scenario scenario = JSONtoObject.getObject(JSONfile);
+        return ScenarioQualityChecker.countAllSteps(scenario.getSteps());
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/keywords-count/{filename}")
+    public int countKeyWordsSteps(@PathVariable String filename) {
+        String JSONfile = new JSONfileReader().toString(filename);
+
+        if(JSONfile == "{}" || JSONfile == "")
+            return 0;
+
+        Scenario scenario = JSONtoObject.getObject(JSONfile);
+        return ScenarioQualityChecker.countKeywordsSteps(scenario.getSteps());
+    }
+
 }
